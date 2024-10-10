c33e4e2b9a6f1b9c12d70dedc1246ee213b2a142
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-10 16:45:33 -0500

    feat(categories): add category entity.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
@@ -0,0 +1,17 @@
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Category()
+    {
+        Name = string.Empty;
+    }
+    
+    public Category(string name)
+    {
+        Name = name;
+    }
+}

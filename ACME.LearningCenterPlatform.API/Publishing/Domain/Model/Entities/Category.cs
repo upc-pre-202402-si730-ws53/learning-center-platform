6e92115540e49d5fa1cfc6606330f507fcdee77b
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-25 17:12:47 -0500

    feat(categories): add relationship to tutorial.

--- ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
@@ -1,9 +1,13 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+
 namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
 
 public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    
+    public ICollection<Tutorial> Tutorials { get; }
 
     public Category()
     {

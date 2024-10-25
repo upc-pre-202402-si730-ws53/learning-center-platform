ad3b12b297efe9670ce4b5cc9d0d65101df3286e
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-25 17:53:37 -0500

    feat(tutorials): add tutorial command service implementation.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Application/Internal/CommandServices/TutorialCommandService.cs
@@ -0,0 +1,33 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Commands;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Services;
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Application.Internal.CommandServices;
+
+public class TutorialCommandService(
+    ITutorialRepository tutorialRepository,
+    ICategoryRepository categoryRepository,
+    IUnitOfWork unitOfWork)
+: ITutorialCommandService
+{
+    public async Task<Tutorial?> Handle(AddVideoAssetToTutorialCommand command)
+    {
+        var tutorial = await tutorialRepository.FindByIdAsync(command.TutorialId);
+        if (tutorial is null) throw new Exception("Tutorial not found");
+        tutorial.AddVideo(command.VideoUrl);
+        await unitOfWork.CompleteAsync();
+        return tutorial;
+    }
+
+    public async Task<Tutorial?> Handle(CreateTutorialCommand command)
+    {
+        var tutorial = new Tutorial(command.Title, command.Summary, command.CategoryId);
+        await tutorialRepository.AddAsync(tutorial);
+        await unitOfWork.CompleteAsync();
+        var category = await categoryRepository.FindByIdAsync(command.CategoryId);
+        tutorial.Category = category;
+        return tutorial;
+    }
+}
--- ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Aggregates/Tutorial.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Aggregates/Tutorial.cs
@@ -7,7 +7,7 @@ public partial class Tutorial
     public int Id { get; }
     public string Title { get; private set; }
     public string Summary { get; private set; }
-    public Category Category { get; private set; }
+    public Category Category { get; internal set; }
     public int CategoryId { get; private set; }
     
     public Tutorial(string title, string summary, int categoryId)

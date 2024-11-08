e51ff236df6746ad6c9c7d70717b887590f0e365
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-08 17:27:21 -0500

    docs: add user repository documentation.

--- ACME.LearningCenterPlatform.API/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
+++ ACME.LearningCenterPlatform.API/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -6,13 +6,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ACME.LearningCenterPlatform.API.IAM.Infrastructure.Persistence.EFC.Repositories;
 
+/// <summary>
+/// User repository implementation. 
+/// </summary>
+/// <param name="context">
+/// The <see cref="AppDbContext"/> database context.
+/// </param>
 public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository
 {
+    // inheritedDoc
     public async Task<User?> FindByUsernameAsync(string username)
     {
         return await Context.Set<User>().FirstOrDefaultAsync(user => user.Username.Equals(username));
     }
 
+    // inheritedDoc
     public bool ExistsByUsername(string username)
     {
         return Context.Set<User>().Any(user => user.Username.Equals(username));
